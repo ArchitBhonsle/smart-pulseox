99565385fca80daa63cc872847cfe55d8963d3ea
Archit Bhonsle <abhonsle2000@gmail.com>
2021-04-15 12:23:50 +0530

    🔗 server and app, fetch records

--- app/lib/your_history.dart
+++ app/lib/your_history.dart
@@ -1,38 +1,49 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class YourHistory extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<Map<String, dynamic>> _getRecords() async {
+  http.Response response = await http.get(Uri.http('192.168.0.111:4000', ''));
+  Map<String, dynamic> body = jsonDecode(response.body);
+
+  return body;
+}
+
+class YourHistory extends StatefulWidget {
+  @override
+  _YourHistoryState createState() => _YourHistoryState();
+}
+
+class _YourHistoryState extends State<YourHistory> {
   @override
   Widget build(BuildContext context) {
-    List<Map<String, dynamic>> _entries = [
-      {
-        'name': 'Archit Bhonsle',
-        'phone': '7498445599',
-        'oxygen': 98,
-      },
-      {
-        'name': 'Sachin Bhonsle',
-        'phone': '7498667799',
-        'oxygen': 95,
-      },
-      {
-        'name': 'Vaishali Bhonsle',
-        'phone': '7498556699',
-        'oxygen': 98,
-      },
-    ];
-
-    return ListView.builder(
-      itemCount: _entries.length,
-      itemBuilder: (context, index) {
-        final entry = _entries[index];
-
-        return Card(
-          child: ListTile(
-            title: Text(entry['name']!),
-            subtitle: Text(entry['phone']!),
-            trailing: Text(entry['oxygen']!.toString()),
-          ),
+    return FutureBuilder<Map<String, dynamic>>(
+      future: _getRecords(), // a previously-obtained Future<String> or null
+      builder:
+          (BuildContext context, AsyncSnapshot<Map<String, dynamic>> snapshot) {
+        Widget child = Center(
+          child: Text('NO DATA YET',
+              style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold)),
         );
+        if (snapshot.hasData) {
+          var _entries = (snapshot.data!['data']);
+          child = ListView.builder(
+            itemCount: _entries.length,
+            itemBuilder: (context, index) {
+              final entry = _entries[index];
+
+              return Card(
+                child: ListTile(
+                  title: Text(entry['name']!),
+                  subtitle: Text(entry['phone']!),
+                  trailing: Text(entry['oxygen']!.toString()),
+                ),
+              );
+            },
+          );
+        }
+        return child;
       },
     );
   }
